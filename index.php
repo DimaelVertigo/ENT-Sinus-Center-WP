23918a557f178242569b2a78d33d83973f8119c4
DimaelVertigo <dimael.vertigo@gmail.com>
2015-07-03 20:44:31 +0300

    Fix contact form2

--- index.php
+++ index.php
@@ -294,7 +294,7 @@ get_header(); ?>
 					</div>
 					<p class="result-msg italic"></p>
 					<div class="contact-form">
-						<?php echo do_shortcode("[contact-form-7 id=6 title=Contact form 1]"); ?>
+						<?php echo do_shortcode("[contact-form-7 id=9 title=Contact form 1]"); ?>
 					</div><!-- contact-form -->
 				</div>
 				
